25e2bda4b5c67dc7e836a8f75f0deea4c11deacf
wurducius <wurducius@gmail.com>
2024-06-30 02:56:50 +0200

    Copy public folder and stylesheets

--- scripts/after-build.js
+++ scripts/after-build.js
@@ -55,3 +55,95 @@ sourceViews.forEach((view, i) => {
   }
   fs.writeFileSync(target, rest);
 });
+
+fs.mkdirSync(path.resolve(PATH_BUILD, "assets", "css"));
+
+sourceViews.forEach((view, i) => {
+  const source = path.resolve(PATH_CWD, "src", "views", view, `${view}.css`);
+  const target = path.resolve(PATH_BUILD, "assets", "css", `${view}.css`);
+
+  if (fs.existsSync(source)) {
+    const styleContent = fs.readFileSync(source);
+    fs.writeFileSync(target, styleContent.toString());
+  }
+});
+
+const checkExistsCreate = (pathToCheck) => {
+  if (!fs.existsSync(pathToCheck)) {
+    fs.mkdirSync(pathToCheck, { recursive: true });
+  }
+};
+
+const parsePublicTree = (node, source, target) => {
+  if (Array.isArray(node)) {
+    node.forEach((child) => {
+      const nextPath = child.split("/")[0];
+      const nodePath = path.resolve(target, nextPath);
+      const sourcePath = path.resolve(source, nextPath);
+      //  checkExistsCreate(target);
+      return parsePublicTree(child, sourcePath, nodePath);
+    });
+  } else {
+    if (node.endsWith(".html") || node.endsWith(".css")) {
+      return;
+    } else {
+      if (source.includes(".")) {
+        const nodeContent = fs.readFileSync(source);
+        console.log(
+          target
+            .split("/")
+            .filter((x, i) => i < x.length)
+            .join("/")
+        );
+        checkExistsCreate(
+          target
+            .split("/")
+            .filter((x, i) => i < x.length)
+            .join("/")
+        );
+        console.log(
+          target
+            .split("/")
+            .filter((x, i) => i < x.length)
+            .join("/")
+        );
+        fs.writeFileSync(target, nodeContent, { recursive: true });
+        // console.log(source);
+        //  console.log(source, target);
+        return;
+      } else {
+        /*
+        return parsePublicTree(
+          node,
+          path.resolve(source, node),
+          path.resolve(target, node)
+        );
+        */
+      }
+    }
+  }
+};
+
+fs.mkdirSync(path.resolve(PATH_BUILD, "fonts"));
+
+const publicTree = fs.readdirSync(path.resolve(PATH_CWD, "public"), {
+  recursive: true,
+});
+publicTree.flat().forEach((x) => {
+  if (!x.includes(".") || x.endsWith(".html") || x.endsWith(".css")) {
+    return;
+  }
+  console.log(x);
+  fs.writeFileSync(
+    path.resolve(PATH_BUILD, x),
+    fs.readFileSync(path.resolve(PATH_CWD, "public", x))
+  );
+});
+
+/*
+const publicTree = fs.readdirSync(path.resolve(PATH_CWD, "public"), {
+  recursive: true,
+});
+const target = path.resolve(PATH_BUILD);
+parsePublicTree(publicTree, path.resolve(PATH_CWD, "public"), target);
+*/
