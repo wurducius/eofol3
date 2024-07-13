54140fbd8a84b45cffa9068f3cc20e58b480ef22
Jakub Elias <elias@acema.cz>
2024-07-13 06:54:24 +0200

    Configure serve script

--- scripts/impl/serve.js
+++ scripts/impl/serve.js
@@ -1,54 +1,34 @@
-const fs = require("fs")
-const { error, spawn } = require("../../eofol/dev-util")
 const { env, paths } = require("../../config")
-
-const { PORT } = env
+const { PORT, HTTPS, BROWSER, HOST } = env
 const { BUILD_PATH } = paths
-
-const SERVE_CACHE_TTL_MS = 31536000000
 
 const liveServer = require("live-server")
 
-const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+// const SERVE_CACHE_TTL_MS = 31536000000
 
-const serve = async (silent, args) => {
-  /*
-  if (fs.existsSync(BUILD_PATH)) {
-    const result = await spawn(
-      "http-server",
-      // @TODO EXTRACT ./build from env paths
-      ["./build", "-p", PORT, "-a", "0.0.0.0", "-g", "-c", SERVE_CACHE_TTL_MS, "--cors", "-o", silent && "-s", args]
-        .flat()
-        .filter(Boolean),
-      {
-        stdio: "inherit",
-      },
-    )
+const SERVE_RELOAD_WAIT_TIME_MS = 250
 
-    return result
-    // process.exit(result.status)
-  } else {
-    console.log(error("Build folder not found. Please run 'npm run build' first."))
-  }
-   */
+const CORS = true
 
-  const params = {
-    port: randomInteger(3000, 8999), // Set the server port. Defaults to 8080.
-    host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
-    root: "./build", // Set root directory that's being served. Defaults to cwd.
-    open: true, // When false, it won't load your browser by default.
-    ignore: "scss,my/templates", // comma-separated string for paths to ignore
-    file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
-    wait: 500, // Waits for all changes, before reloading. Defaults to 0 sec.
-    //  mount: [["./node_modules"]], // Mount a directory to a route.
-    logLevel: 1, // 0 = errors only, 1 = some, 2 = lots
-    middleware: [
-      function (req, res, next) {
-        next()
-      },
-    ], // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
-  }
-  liveServer.start(params)
+const serveOptions = {
+  port: PORT,
+  host: HOST,
+  root: BUILD_PATH,
+  open: true,
+  file: "index.html",
+  wait: SERVE_RELOAD_WAIT_TIME_MS,
+  logLevel: 0,
+  cors: CORS,
+  https: HTTPS === "https",
+  middleware: [
+    (req, res, next) => {
+      next()
+    },
+  ],
+}
+
+const serve = () => {
+  liveServer.start(serveOptions)
 }
 
 module.exports = serve
--- scripts/serve.js
+++ scripts/serve.js
@@ -3,5 +3,4 @@ const { primary } = require("../eofol/dev-util")
 
 console.log(primary("Serving build folder..."))
 
-const args = process.argv.slice(2)
-serve(false, args)
+serve()
