2a83a4860fd1f69d35c73580b8c27b91fcc57b91
Jakub Elias <elias@acema.cz>
2024-07-17 20:21:27 +0200

    Eslint ts

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -1,11 +1,15 @@
+// eslint-disable-next-line no-undef
 const tsParser = require("@typescript-eslint/parser")
+// eslint-disable-next-line no-undef
 const tsPlugin = require("@typescript-eslint/eslint-plugin")
+// eslint-disable-next-line no-undef
 const eslintJS = require("@eslint/js")
 
+// eslint-disable-next-line no-undef
 module.exports = [
   {
     files: ["**/*.js", "**/*.ts"],
-    ignores: ["derived/**", "dist/**", "dist2/**", "build/**", ".idea/**", "node_modules/**", "*.d.ts", "src/**"],
+    ignores: ["derived/**", "dist/**", "dist2/**", "build/**", ".idea/**", "node_modules/**", "*.d.ts"],
     rules: {
       ...eslintJS.configs.recommended.rules,
       semi: ["error", "never"],
@@ -14,14 +18,14 @@ module.exports = [
     },
     languageOptions: {
       parser: tsParser,
-      sourceType: "commonjs",
+      sourceType: "module",
       globals: {
         process: true,
         console: true,
         window: true,
-        self: true,
-        caches: true,
-        fetch: true,
+        structuredClone: true,
+        document: true,
+        navigator: true,
       },
     },
     plugins: { "@typescript-eslint": tsPlugin },
