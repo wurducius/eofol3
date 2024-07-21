09225b7979fb56abd93696daead4c624cf5be51e
wurducius <wurducius@gmail.com>
2024-07-21 12:32:30 +0200

    Add compile less styles

--- eofol/compiler/style.js
+++ eofol/compiler/style.js
@@ -1,22 +1,35 @@
 const fs = require("fs")
 const path = require("path")
-
-const { PATH_VIEWS_SRC, EXT_CSS } = require("../constants")
+const less = require("less")
+const { PATH_VIEWS_SRC, EXT_LESS, EXT_CSS } = require("../constants")
 const { relativizeStylesheet } = require("./relativize")
 
 const CODE_STYLE_TAG_END = "</style>"
 
-const compileStyle = (view, stylesSx) => (htmlPage) => {
-  const source = path.resolve(PATH_VIEWS_SRC, view, `${view}${EXT_CSS}`)
+const injectStyles = (htmlPage, styles) =>
+  htmlPage
+    .split(CODE_STYLE_TAG_END)
+    .map((htmlPart, i) => (i === 0 ? `${htmlPart} ${relativizeStylesheet(styles)}${CODE_STYLE_TAG_END}` : htmlPart))
+    .join("")
 
-  if (fs.existsSync(source)) {
-    const stylesContent = relativizeStylesheet(fs.readFileSync(source).toString() + stylesSx)
-    return htmlPage
-      .split(CODE_STYLE_TAG_END)
-      .map((htmlPart, i) => (i === 0 ? `${htmlPart} ${stylesContent}${CODE_STYLE_TAG_END}` : htmlPart))
-      .join("")
+const compileStyle = (view, stylesSxList) => (htmlPage) => {
+  const lessPath = path.resolve(PATH_VIEWS_SRC, view, `${view}${EXT_LESS}`)
+  const cssPath = path.resolve(PATH_VIEWS_SRC, view, `${view}${EXT_CSS}`)
+  const stylesSx = stylesSxList.join(" ")
+
+  if (fs.existsSync(lessPath)) {
+    return less
+      .render(fs.readFileSync(lessPath).toString(), {})
+      .then((derivedStyles) =>
+        injectStyles(
+          htmlPage,
+          derivedStyles.css + (fs.existsSync(cssPath) ? fs.readFileSync(cssPath).toString() : "") + stylesSx,
+        ),
+      )
+  } else if (fs.existsSync(cssPath)) {
+    return injectStyles(htmlPage, fs.readFileSync(cssPath).toString() + stylesSx)
   } else {
-    return undefined
+    return injectStyles(htmlPage, stylesSx)
   }
 }
 
--- eofol/constants/common.js
+++ eofol/constants/common.js
@@ -5,5 +5,6 @@ const EXT_PNG = ".png"
 const EXT_JPG = ".jpg"
 const EXT_JPEG = ".jpeg"
 const EXT_GZIP = ".gz"
+const EXT_LESS = ".less"
 
-module.exports = { EXT_JS, EXT_CSS, EXT_HTML, EXT_PNG, EXT_JPG, EXT_JPEG, EXT_GZIP }
+module.exports = { EXT_JS, EXT_CSS, EXT_HTML, EXT_PNG, EXT_JPG, EXT_JPEG, EXT_GZIP, EXT_LESS }
