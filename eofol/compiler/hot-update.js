46e733e8c63749070a0c91c0eff02cc62d31d388
wurducius <wurducius@gmail.com>
2024-08-07 03:06:49 +0200

    Cache hot update checksums

--- eofol/compiler/hot-update.js
+++ eofol/compiler/hot-update.js
@@ -2,18 +2,32 @@ const fs = require("fs")
 const checksum = require("./checksum")
 const { primary } = require("../dev-util")
 
+const cache = {}
+
 const hotUpdate = (isHot, targetPath, sourcePath, nextContent, coldHandler) => {
   const writeFile = () => {
     fs.writeFileSync(targetPath, nextContent)
   }
 
   if (isHot && fs.existsSync(targetPath)) {
-    const prevChecksum = checksum(fs.readFileSync(targetPath).toString())
+    const cached = cache[targetPath]
+
+    let prevChecksum
+    if (cached) {
+      prevChecksum = cached
+    } else {
+      prevChecksum = checksum(fs.readFileSync(targetPath).toString())
+    }
     const nextChecksum = checksum(nextContent.toString())
 
     if (prevChecksum !== nextChecksum) {
       console.log(primary(`Updating ${targetPath}`))
+      cache[targetPath] = nextChecksum
       writeFile()
+    } else {
+      if (!cached) {
+        cache[targetPath] = prevChecksum
+      }
     }
   } else {
     writeFile()
