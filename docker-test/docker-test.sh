c155643f174346f20bbf22b9dbf5158f396c1e3f
wurducius <wurducius@gmail.com>
2024-08-08 02:05:43 +0200

    Separate docker-run script into lines, lint project

--- docker-test/docker-test.sh
+++ docker-test/docker-test.sh
@@ -1,1 +1,10 @@
-echo "Eofol3 Docker test" && echo "[1/3] Cloning Eofol3..." && git clone https://github.com/wurducius/eofol3 && cd eofol3 && echo "[2/3] Installing Eofol3..." && npm i && echo "[3/3] Running Eofol3..." && npm start
+#!/bin/bash
+
+echo "Eofol3 Docker test"
+echo "[1/3] Cloning Eofol3..."
+git clone https://github.com/wurducius/eofol3
+cd eofol3
+echo "[2/3] Installing Eofol3..."
+npm i
+echo "[3/3] Running Eofol3..."
+npm start
